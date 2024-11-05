38fbb9e0b2e9042fd9eff88a089c92d86b3da203
Matthew Clonch <matthewrclonch@gmail.com>
2024-11-05 13:33:56 -0600

    Revert "Login/Auth setup with basic ApplicationViews setup"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import "./styles/App.css";
-import { Login } from "./components/auth/Login";
-import { ApplicationViews } from "./views/ApplicationViews";
-import { Authorized } from "./views/Authorized";
+import "./App.css";
 
-export const App = () => {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route
-        path="*"
-        element={
-          <Authorized>
-            <ApplicationViews />
-          </Authorized>
-        }
-      ></Route>
-    </Routes>
-  );
-};
+function App() {
+  return <div className="App">Shepherd's Pies</div>;
+}
+
+export default App;
