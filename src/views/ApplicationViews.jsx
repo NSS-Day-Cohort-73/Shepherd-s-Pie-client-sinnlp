097d0084e60f00a148ec48d8021b2eade4f74165
Niles Clancy <nilesclancy@gmail.com>
2024-11-12 11:04:28 -0500

    addedEmployees+SalesReportsToAuthOnly

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -16,7 +16,6 @@ export const ApplicationViews = () => {
   useEffect(() => {
     const localPieUser = localStorage.getItem("shepherds_pie_user");
     const pieUserObject = JSON.parse(localPieUser);
-
     setCurrentUser(pieUserObject);
   }, []);
 
@@ -26,7 +25,7 @@ export const ApplicationViews = () => {
         path="/"
         element={
           <>
-            <NavBar />
+            <NavBar currentUser={currentUser} /> {/* Passing currentUser */}
             <Outlet />
           </>
         }
@@ -35,12 +34,11 @@ export const ApplicationViews = () => {
           path="create-order"
           element={<CreateOrder currentUser={currentUser} />}
         />
-        <Route path="sales-report" element={<SalesReport />} />
-        <Route path="/add-pizza" element={<AddPizza />} />
         <Route
           path="all-orders"
           element={<AllOrders currentUser={currentUser} />}
         />
+
         <Route
           path="orders/:orderId"
           element={<OrderDetails currentUser={currentUser} />}
@@ -49,17 +47,93 @@ export const ApplicationViews = () => {
           path="orders/:orderId/add-pizza"
           element={<AddPizza currentUser={currentUser} />}
         />
-        <Route
+        {/* <Route
           path="all-employees"
           element={<AllEmployees currentUser={currentUser} />}
-        />
+        /> */}
         <Route
           path="update-employee/:employeeId"
           element={<UpdateEmployees currentUser={currentUser} />}
         />
+        {currentUser?.isAdmin && (
+          <>
+            <Route
+              path="all-employees"
+              element={<AllEmployees currentUser={currentUser} />}
+            />
+            <Route
+              path="sales-report"
+              element={<SalesReport currentUser={currentUser} />}
+            />
+          </>
+        )}
       </Route>
-      {/* <Route path="/orders" element={<Orders />} /> */}
-      {/* not useful yet */}
     </Routes>
   );
 };
+
+// import { useEffect, useState } from "react";
+// import { Outlet, Route, Routes } from "react-router-dom";
+// import { CreateOrder } from "../components/orders/Createorders";
+// import { AddPizza } from "../components/pizzas/addPizza";
+// //eventually we'll need an orders.jsx import for the order details view
+// import { NavBar } from "../components/nav/navBar";
+// import { OrderDetails } from "../components/orders/OrderDetails";
+// import { SalesReport } from "../components/salesreport/SalesReport";
+// import { AllOrders } from "../components/orders/AllOrders";
+// import { AllEmployees } from "../components/employees/AllEmployees";
+// import { UpdateEmployees } from "../components/employees/UpdateEmployees";
+
+// export const ApplicationViews = () => {
+//   const [currentUser, setCurrentUser] = useState({});
+
+//   useEffect(() => {
+//     const localPieUser = localStorage.getItem("shepherds_pie_user");
+//     const pieUserObject = JSON.parse(localPieUser);
+
+//     setCurrentUser(pieUserObject);
+//   }, []);
+
+//   return (
+//     <Routes>
+//       <Route
+//         path="/"
+//         element={
+//           <>
+//             <NavBar />
+//             <Outlet />
+//           </>
+//         }
+//       >
+//         <Route
+//           path="create-order"
+//           element={<CreateOrder currentUser={currentUser} />}
+//         />
+//         <Route path="sales-report" element={<SalesReport />} />
+//         <Route path="/add-pizza" element={<AddPizza />} />
+//         <Route
+//           path="all-orders"
+//           element={<AllOrders currentUser={currentUser} />}
+//         />
+//         <Route
+//           path="orders/:orderId"
+//           element={<OrderDetails currentUser={currentUser} />}
+//         />
+//         <Route
+//           path="orders/:orderId/add-pizza"
+//           element={<AddPizza currentUser={currentUser} />}
+//         />
+//         <Route
+//           path="all-employees"
+//           element={<AllEmployees currentUser={currentUser} />}
+//         />
+//         <Route
+//           path="update-employee/:employeeId"
+//           element={<UpdateEmployees currentUser={currentUser} />}
+//         />
+//       </Route>
+//       {/* <Route path="/orders" element={<Orders />} /> */}
+//       {/* not useful yet */}
+//     </Routes>
+//   );
+// };
